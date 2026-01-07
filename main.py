43d9f98cbfe0d5a5ab05a77161af9df21a8f36fd
Snivellus Snape <21307140051@m.fudan.edu.cn>
2026-01-07 21:00:24 +0800

    Update

--- main.py
+++ main.py
@@ -25,9 +25,16 @@ class SeleniumAuth:
         
         options = Options()
         options.add_argument("--headless=new")
+        options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=options)
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--lang=zh-CN")
+
+        driver = webdriver.Chrome(
+            service=Service("/usr/bin/chromedriver"),
+            options=options
+        )
 
         try:
             driver.get(self.LOGIN_URL)
